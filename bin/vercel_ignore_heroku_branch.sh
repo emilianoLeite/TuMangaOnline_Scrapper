1ee264855818eac3412da1707001a6773a66323d
Emiliano Leite <emiliano.leite@creditas.com.br>
2021-07-20 12:17:21 -0300

    Update message of vercel_ignore_heroku_branch.sh

--- bin/vercel_ignore_heroku_branch.sh
+++ bin/vercel_ignore_heroku_branch.sh
@@ -4,10 +4,9 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "heroku" ]] ; then
   # Don't build
-  echo "🛑 - Build cancelled"
+  echo "🛑 - Actual branch is 'heroku'. Build cancelled"
   exit 0;
 else
   # Proceed with the build
-  echo "✅ - Build can proceed"
   exit 1;
 fi
